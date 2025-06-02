c587d1b4e4616ecd5778ff892d47ae4abd0f3235
DimiDev <dimidev@olympusone.com>
2025-06-02 19:16:36 +0300

    Refactor PaymentMethodDecorator by removing unused available_for_order? method and update version to 2.1.0

--- app/models/spree/payment_method_decorator.rb
+++ app/models/spree/payment_method_decorator.rb
@@ -1,13 +1,11 @@
 module Spree
   module PaymentMethodDecorator
-    def available_for_order?(order)
-      return cod_payment? && super if order.shipping_method&.cod?
-
-      !cod_payment? && super
-    end
-
     def cod_payment?
       method_type == 'cod_payment'
+    end
+
+    def cod_payment_available?(order)
+      order.shipping_method&.cod?
     end
   end
 end
