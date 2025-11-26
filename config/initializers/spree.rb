6009622316ce5bdebd8db031883b2dfe995ecd95
DimiDev <dimidev@olympusone.com>
2025-11-26 11:21:27 +0200

    Add comment for clarity in spree.rb initializer

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,6 +1,7 @@
 Rails.application.config.after_initialize do
   Rails.application.config.spree.payment_methods << Spree::PaymentMethod::CodPayment
 
+  # Admin partials
   Rails.application.config.spree_admin.shipping_method_form_partials << 'spree/admin/shipping_methods/cod_form'
 
   Spree::PermittedAttributes.shipping_method_attributes << :cod
